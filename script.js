d6f40f7d184ff695a3c1087151bcd981f506b008
rbohar11 <rajeevbohara237@gmail.com>
2021-12-28 10:15:09 +0800

    Some basic fundamentals

---
+++ script.js
@@ -0,0 +1,78 @@
+/*let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+
+// let firstname = "Jonas";
+// console.log(firstname);
+// console.log(firstname);
+// console.log(firstname);
+
+//Variable name conventions
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+console.log(myCurrentJob);
+console.log(myFirstJob);
+true;
+console.log(true);
+console.log(typeof true);
+
+let javaScriptIsNice = true;
+console.log(javaScriptIsNice);
+javaScriptIsNice = 'YES!';
+console.log(typeof javaScriptIsNice);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year); 
+*/
+
+/*
+let age = 30;
+console.log('job');
+
+const birthYear = 1991;
+// birthYear = 1991;
+
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+*/
+const now = 2037;
+const ageJonas = now - 1998;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+/*
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+const firstName = 'Jonas';
+const lastName = 'Ethens';
+console.log(firstName + ' ' + lastName);
+let x = 10 + 5;
+x += 10; // x = x + 10
+x *= 4; //x = x * 4
+x++; // x = x + 1
+console.log(x);
+*/
+//Comparision operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 35);
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
+
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge);
+
+
